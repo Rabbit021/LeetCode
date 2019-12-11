428808499859da858956ff63d4088a88e186e7a6
wangheng <635253084@qq.com>
2019-12-11 12:37:43 +0800

    两数之和

---
+++ LeetCode/LeetCode/TowSum/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.TowSum
+{
+    public class Solution
+    {
+        /// <summary>
+        /// 两次循环,超时
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (i != j && nums[i] + nums[j] == target)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+            return new int[] { };
+        }
+
+        /// <summary>
+        /// 第1次优化
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSumV2(int[] nums, int target)
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var data2 = target - nums[i];
+                if (dict.ContainsKey(data2))
+                    return new[] { dict[data2], i };
+                dict[nums[i]] = i;
+            }
+            return new int[] { };
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/TowSum/SolutionTests.cs
@@ -0,0 +1,61 @@
+﻿using LeetCode.TowSum;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.TowSum.Tests
+{
+    [TestCategory("两数之和")]
+    [TestClass]
+    public class SolutionTests
+    {
+        private List<TestData> datas;
+
+        [TestInitialize]
+        public void Intlize()
+        {
+            datas = new List<TestData>
+            {
+                new TestData {Input = new[] {2, 5, 5, 11}, Target = 10, Result = new[] {1, 2}},
+                new TestData {Input = new[] {2, 7, 11, 15}, Target = 9, Result = new[] {0, 1}}
+            };
+        }
+
+        [TestMethod]
+        public void TwoSumTest()
+        {
+            var sln = new Solution();
+            foreach (var itr in datas)
+            {
+                var rst = sln.TwoSum(itr.Input, itr.Target);
+                Vaildate(rst, itr.Result);
+            }
+        }
+
+        [TestMethod()]
+        public void TwoSumV2Test()
+        {
+            var sln = new Solution();
+            foreach (var itr in datas)
+            {
+                var rst = sln.TwoSumV2(itr.Input, itr.Target);
+                Vaildate(rst, itr.Result);
+            }
+        }
+
+        private void Vaildate(int[] result, int[] succes)
+        {
+            for (var i = 0; i < succes.Length; i++)
+                if (succes[i] != result[i])
+                    Assert.Fail();
+        }
+
+        public class TestData
+        {
+            public int[] Input;
+            public int[] Result;
+            public int Target;
+        }
+
+       
+    }
+}
