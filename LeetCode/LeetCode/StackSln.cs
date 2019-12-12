85037020c5c63e2d62ef9bef6e820b27481f94bf
wangheng <635253084@qq.com>
2019-12-12 15:45:18 +0800

    有效括号问题

---
+++ LeetCode/LeetCode/StackSln.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 栈类型的算法
+    /// </summary>
+    public class StackSln
+    {
+        /// <summary>
+        /// #20 有效括号问题
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid(string s)
+        {
+            var stack = new Stack<char>();
+            var dict = new Dictionary<char, char>()
+            {
+                { '(',')'},
+                { '[',']'},
+                { '{','}'}
+            };
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    stack.Push(s[i]);
+                }
+                else
+                {
+                    if (!stack.Any())
+                        return false;
+                    var top = stack.Pop();
+                    if (dict[top] != s[i])
+                        return false;
+                }
+            }
+            return !stack.Any();
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/StackSlnTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class StackSlnTests
+    {
+        [TestMethod()]
+        public void IsValidTest()
+        {
+            var sln = new StackSln();
+
+            var rst = sln.IsValid("()");
+            Assert.AreEqual(rst, true);
+
+            rst = sln.IsValid("()[]{}");
+            Assert.AreEqual(rst, true);
+
+            rst = sln.IsValid("(]");
+            Assert.AreEqual(rst, false);
+
+            rst = sln.IsValid("([)]");
+            Assert.AreEqual(rst, false);
+
+            rst = sln.IsValid("{[]}");
+            Assert.AreEqual(rst, true);
+
+            rst = sln.IsValid("]");
+            Assert.AreEqual(rst, false);
+        }
+    }
+}
